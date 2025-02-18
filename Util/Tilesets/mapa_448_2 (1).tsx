630d406a7a253f9b922365919e3d65b35c8f0fbf
Fábio Júlio <fabiojulio.santos@protonmail.ch>
2025-02-17 22:40:33 -0300

    Adição Util

---
+++ Util/Tilesets/mapa_448_2 (1).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="mapa_448_2 (1)" tilewidth="8" tileheight="8" tilecount="900" columns="36">
+ <image source="../../../../../Workspace/ifpeJogo/pibex-jogo-campus-paulista/projeto/assets/backgrounds/mapa_448_2 (1).png" width="288" height="200"/>
+</tileset>
